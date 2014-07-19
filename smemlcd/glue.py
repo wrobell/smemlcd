ef5993ced3fee7e45f3cbaa9c11ebd8f6d1ab9ce
wrobell <wrobell@pld-linux.org>
2014-07-19 20:39:39 +0100

    - added python module for the sharp memory lcd library

---
+++ smemlcd/glue.py
@@ -0,0 +1,51 @@
+#
+# smemlcd - Sharp Memory LCDs library
+#
+# Copyright (C) 2013 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Sharp Memory LCDs library.
+"""
+
+import ctypes as ct
+
+class SMemLCD(object):
+    """
+    Sharp Memory LCDs display class.
+    """
+    def __init__(self):
+        """
+        Create Sharp Memory LCD display instance.
+        """
+        self._lib = ct.CDLL('libmemlcd.so.0')
+        self._lib.smemlcd_init()
+
+
+    def write(self, data):
+        """
+        Write data to Sharp memory LCD.
+
+        :param data: Screen data to display.
+        """
+        n = len(data)
+        assert n == 12000
+
+        buff = (ct.c_ubyte * n).from_buffer_copy(painter.data)
+        self._lib.memlcd_write(buff)
+
+
+# vim: sw=4:et:ai
